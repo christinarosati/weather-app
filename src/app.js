4f4d4ab3b2c40fabecbee85d99bd6159631d1d0b
christinarosati <chrisrosati22@gmail.com>
2022-09-22 13:08:36 -0400

    Added relevant weather icon with API

--- src/app.js
+++ src/app.js
@@ -41,6 +41,15 @@ function showCityTemperature(response) {
   document.querySelector("#day-time").innerHTML = formatDate(
     response.data.dt * 1000
   );
+  document
+    .querySelector("#icon")
+    .setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+  document
+    .querySelector("#icon")
+    .setAttribute("alt", response.data.weather[0].description);
 }
 
 function showPosition(position) {
